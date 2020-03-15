9189fa3431406571b3a7f0f728aaa3d764341b95
John Rhey Damian <johnr.damian98@gmail.com>
2020-03-15 15:35:35 +0800

    simple hugo website

--- static/scripts/index.js
+++
@@ -1,7 +0,0 @@
-import Cube from './library/cube';
-import Program from './library/main';
-
-let current_program = null;
-
-current_program = Program.GetInstance(new Cube());
-current_program.Main();
